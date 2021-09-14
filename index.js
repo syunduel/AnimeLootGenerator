e7b038f002d4020fcfec6f5de39a52bd9b132bf3
syunduel <5934746+syunduel@users.noreply.github.com>
2021-09-14 23:51:56 +0900

    ファイル名を変更

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const sharp = require('sharp');
     // 【Node.js】 画像処理モジュールsharpの使い方を網羅してみました
     // https://note.affi-sapo-sv.com/nodejs-sharp.php
 
-    await sharp('resources/01 顔　F.png')
+    await sharp('resources/1faceM1.png')
            .composite([ 
                   {
-                    input: 'resources/02　目　１.png',
+                    input: 'resources/2eye3.png',
                     // top: 50,
                     // left: 100
                 }, {
-                    input: 'resources/03 髪１.png',
+                    input: 'resources/3hair2.png',
                     // top: 100,
                     // left: 200
                 // },{
@@ -22,7 +22,7 @@ const sharp = require('sharp');
                 //     left: 200
                 },
            ] )
-           .toFile('dist/output2.png', (err, info)=>{
+           .toFile('dist/outputM132.png', (err, info)=>{
             if(err){ throw err }
             });
     
